ae73b9a5b13e483d7b9e4db13fd18b8e5ab56887
org-lib@163.com <deqiao.yuan@baozun.com>
2025-01-03 14:04:28 +0800

    migrate

--- example/parser/main.go
+++ example/parser/main.go
@@ -43,7 +43,7 @@ func ParseSQL(sql string) []string {
 // github.com/akito0107/xsqlparser 支持with 语法
 func main() {
 	_ = logger.InitLogger(logger.LoggerConfig{})
-	sql1 := "SELECT * FROM table1;"
+	sql1 := "SELECT inv.id AS id, inv.version AS version, inv.im_organization AS imOrganization, inv.sku_code AS skuCode, inv.inv_status_code AS invStatusCode , inv.location_code AS locationCode, inv.cw_code AS cwCode, inv.quota_interval AS quotaInterval, inv.create_time AS createTime, inv.last_modify_time AS lastModifyTime , inv.`qty` AS qty, SUM(CASE WHEN occ.current_occupy_qty IS NULL THEN 0 ELSE occ.current_occupy_qty END) AS occupyQty , inv.qty + SUM(CASE WHEN occ.current_occupy_qty IS NULL THEN 0 ELSE occ.current_occupy_qty END) AS availableQty, inv.saas_tenant_code AS saasTenantCode FROM ( SELECT * FROM t_cloud_location_inventory_6 inv1 WHERE inv1.im_organization = 'JackWolfskinGG' AND inv1.saas_tenant_code = 'JACKWOLFSKIN' ORDER BY inv1.id ASC LIMIT 0, 50000 ) inv LEFT JOIN t_cloud_inventory_occupy_6 occ ON inv.im_organization = occ.im_organization AND inv.sku_code = occ.sku_code AND inv.inv_status_code = occ.inv_status_code AND inv.cw_code = occ.cw_code AND inv.location_code = occ.location_code AND inv.quota_interval = occ.quota_interval AND occ.saas_tenant_code = 'JACKWOLFSKIN' GROUP BY inv.im_organization, inv.sku_code, inv.inv_status_code, inv.cw_code, inv.location_code, inv.quota_interval ORDER BY inv.id ASC"
 	fmt.Println(ParseSQL(sql1))
 
 	sql2 := "INSERT INTO table2 VALUES (1, 'test');"
@@ -61,7 +61,7 @@ func main() {
 	sql6 := "DROP TABLE table6;"
 	fmt.Println(ParseSQL(sql6))
 
-	sql7 := "SELECT inv.`customer_code` AS customerCode, inv.`owner_code` AS ownerCode, inv.`bin_code` AS binCode, inv.`sku_code` AS skuCode, inv.`inv_status_code` AS invStatusCode , inv.`location_code` AS locationCode, inv.quota_interval AS quotaInterval, inv.`qty` AS qty , CASE WHEN SUM(occ.`qty`) != '' THEN SUM(occ.`qty`) ELSE 0 END AS occupyQty , CASE WHEN SUM(occ.`qty`) != '' THEN inv.`qty` + SUM(occ.`qty`) ELSE inv.`qty` END AS availableQty FROM ( SELECT t.id AS id, t.`customer_code` AS customer_code, t.`owner_code` AS owner_code, t.`bin_code` AS bin_code, t.`sku_code` AS sku_code , t.`inv_status_code` AS inv_status_code, t.`location_code` AS location_code, t.quota_interval AS quota_interval, t.`qty` AS qty FROM inv_customer_inventory_63 t WHERE t.customer_code = 'PUMAKG' AND t.owner_code IN ('PUMA官方店', 'PUMA_YY0I01') AND t.bin_code IN ('SHWH430', 'SHWH512', 'PUMA_YY_0001', 'xxx') AND t.inv_status_code = '10' AND t.qty >= 1 AND t.saas_tenant_code = 'baozun' ORDER BY t.id ASC LIMIT 0, 20 ) inv LEFT JOIN inv_occupy_63 occ ON occ.`bin_code` = inv.`bin_code` AND occ.`sku_code` = inv.`sku_code` AND occ.`inv_status_code` = inv.`inv_status_code` AND occ.`location_code` = inv.`location_code` AND occ.`customer_code` = inv.`customer_code` AND occ.`owner_code` = inv.`owner_code` AND occ.`quota_interval` = inv.`quota_interval` AND occ.`qty` < 0 AND occ.saas_tenant_code = 'baozun' GROUP BY inv.`sku_code`, inv.`owner_code`, inv.`customer_code`, inv.`bin_code`, inv.`location_code`, inv.`inv_status_code`, inv.quota_interval"
+	sql7 := "ALTER TABLE table7 ADD COLUMN column1 varchar(255);"
 	fmt.Println(ParseSQL(sql7))
 	zap.L().Info("aaaaa....")
 }
